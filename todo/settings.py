ff1aae884f9661b69a1b1ce85cad13a9226c3f44
Stepan <workwr@yandex.ru>
2019-01-27 17:56:42 +0500

    add local settings

--- todo/settings.py
+++ todo/settings.py
@@ -6,3 +6,9 @@ DEFAULT_LIST_ID = getattr(settings, 'TODO_DEFAULT_LIST_ID', 1)
 DEFAULT_ASSIGNEE = getattr(settings, 'TODO_DEFAULT_ASSIGNEE', None)
 PUBLIC_SUBMIT_REDIRECT = getattr(settings, 'TODO_PUBLIC_SUBMIT_REDIRECT', '/')
 SITE_DOMAIN = getattr(settings, 'TODO_SITE_DOMAIN', 'crm.osc-group.ru')
+
+try:
+    from todo.local_settings import *
+except ImportError:
+    pass
+
