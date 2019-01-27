f71e02a75d60e34544e1790accb082b7a6fbb964
Stepan <workwr@yandex.ru>
2019-01-27 18:06:34 +0500

    fix

--- todo/settings.py
+++ todo/settings.py
@@ -5,7 +5,7 @@ STAFF_ONLY = getattr(settings, 'TODO_STAFF_ONLY', False)
 DEFAULT_LIST_ID = getattr(settings, 'TODO_DEFAULT_LIST_ID', 1)
 DEFAULT_ASSIGNEE = getattr(settings, 'TODO_DEFAULT_ASSIGNEE', None)
 PUBLIC_SUBMIT_REDIRECT = getattr(settings, 'TODO_PUBLIC_SUBMIT_REDIRECT', '/')
-SITE_DOMAIN = getattr(settings, 'TODO_SITE_DOMAIN', 'crm.osc-group.ru')
+SITE_DOMAIN = getattr(settings, 'TODO_SITE_DOMAIN', 'example.com')
 
 try:
     from todo.local_settings import *
